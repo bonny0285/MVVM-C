abe3c1bcdf7820ae0c5e5997d3ba66199e4c00ae
Massimiliano Bonafede <massimiliano.bonafede@altran.it>
2021-05-08 16:31:50 +0200

    Created : - LoginViewController - RegistrationViewController - RecoveryPasswordViewController

---
+++ MVVM-C/Scenes/Login/ViewControllers/LoginVIewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginVIewController.swift
+//  MVVM-C
+//
+//  Created by Bonafede Massimiliano on 08/05/21.
+//
+
+import UIKit
+
+class LoginVIewController: BaseViewController {
+
+}
---
+++ MVVM-C/Scenes/Login/ViewControllers/RecoveryPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecoveryPasswordViewController.swift
+//  MVVM-C
+//
+//  Created by Bonafede Massimiliano on 08/05/21.
+//
+
+import UIKit
+
+class RecoveryPasswordViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
