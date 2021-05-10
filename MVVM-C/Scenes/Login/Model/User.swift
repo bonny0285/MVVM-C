d60bcafcce29350c69aa44799e0cbb1b3a04f54e
Massimiliano <massimiliano.bonafede@altran.it>
2021-05-10 08:56:41 +0200

    Created DataBaseLogin class

--- MVVM-C/Scenes/Login/Model/User.swift
+++ MVVM-C/Scenes/Login/Model/User.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+enum UserError: String, Error {
+    case userAlreadySaved = "User already saved"
+}
 
-class User {
+class User: Codable, Equatable {
+    
+    
     var username: String
     var password: String
     var isCurrentAccount: Bool
@@ -18,4 +23,73 @@ class User {
         self.password = password
         self.isCurrentAccount = isCurrentAccount
     }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.username == rhs.username
+    }
 }
+
+
+class DataBaseLogin {
+    
+    private let userDefault = UserDefaults.standard
+    
+    private var usersContainer: [String : User] = [:]
+    
+    init() {
+        usersContainer = userDefault.array(forKey: "users") as? [String : User] ?? [:]
+    }
+    
+    
+    
+    
+    private func saveUser() {}
+    
+    func saveUser(_ user: User, completion: (Result<Bool,UserError>) -> Void) {
+        
+        if let _ = usersContainer["\(user.username)"] {
+            // Gestire in caso di utente gia salvato
+            completion(.failure(.userAlreadySaved))
+        } else {
+            // Gestire in caso di utente non presente nel DB
+            usersContainer["\(user.username)"] = user
+            
+            //do {
+                //let encoded = try JSONEncoder().encode(user)
+                //userDefault.set(encoded, forKey: "user\(user.username)")
+                //userDefault.synchronize()
+                
+            //} catch let error {
+              //  print(error.localizedDescription)
+            //}
+        }
+    }
+    
+    func retriveUser(_ user: User) -> User {
+        var result: User?
+        
+        if let user = userDefault.object(forKey: "user\(user.username)") as? Data {
+        
+            do {
+                result = try JSONDecoder().decode(User.self, from: user)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        return result ?? User(username: "", password: "", isCurrentAccount: false)
+    }
+}
+
+
+/*
+ 
+ Processo di salvataggio:
+ 
+ - Controllare che il nuovo user non è gia presente nel container altrimenti mostrare un alert di errore, gestire il caso con un closure che ritorna failure.
+ - In caso che il nuovo user non è presente mostrare un alert di avvenuta registrazione, gestire il caso con una closure che ritorna success
+ 
+ Processo di retrive:
+ 
+ - Controllare che l'user sia presente e gestire il caso con una closure che ritarna l'user in caso di success oppure failure in caso contrario
+ 
+ */
